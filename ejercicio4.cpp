692f6f358fdd0da02bba5370d16cdf5f55842d9b
josue <josue.mlpro.2014@gmail.com>
2025-06-12 11:38:31 -0500

    ejercicio_4-avance_1-obteniendo datos

---
+++ ejercicio4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	
+	int a, b, c;
+	
+	cout<<ingrese el coeficiente de la variable de grado 2 <<endl;
+	cin>> a;
+	
+	cout<<ingrese el coeficiente de la variable de grado 1 <<endl;
+	cin>> b;
+	cout<<ingrese el termino independiente <<endl;
+	cin>> c;
+	
+	
+}
