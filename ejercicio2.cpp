d63bf93cd7e5c789471d2163e19224113a6e86e6
josue <josue.mlpro.2014@gmail.com>
2025-06-11 23:23:13 -0500

    ejercicio_2-avance_1-pidiendo datos

---
+++ ejercicio2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+	
+	int cn, mv;
+	cout<<ingrese la cantidad de numeros aleatorios que desea generar <<endl;
+	cin>> cn;
+	
+	cout<<ingrese el maximo valor que puede tener el numero <<endl;
+	cin>> mv;
+	
+	
+	
+	return 0;
+}
