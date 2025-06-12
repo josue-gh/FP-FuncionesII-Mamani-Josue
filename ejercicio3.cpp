fca82bf49d13df5f395b1056285ee69c4f65f298
josue <josue.mlpro.2014@gmail.com>
2025-06-12 11:04:25 -0500

    ejercicio_3-avance_1-pidiendo datos y elevando al cuadrado

---
+++ ejercicio3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	int c1, c2, sc;
+	
+	cout<<ingrese el primer cateto  <<endl;
+	cin>> c1;
+	
+	cout<<ingrese el segundo cateto <<endl;
+	cin>> c2;
+	
+	sc= pow(c1, 2) + pow(c2, 2) ;
+	
+	return 0;
+}
