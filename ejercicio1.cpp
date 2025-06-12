86b93366ce9419db1f0abaeba0db94d2d812bb94
josue <josue.mlpro.2014@gmail.com>
2025-06-11 23:03:29 -0500

    ejercicio_1-avance_1-pidiendo datos y asignando valores

---
+++ ejercicio1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+	
+	
+int main(){
+	float r, pi, a;
+	cout<<ingrese el radio del circulo <<endl;
+	cin>> r;
+	
+	pi= M_PI;
+	
+
+	
+	return 0;
+}
