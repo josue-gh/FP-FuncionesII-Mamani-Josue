66c5074b690f36483fd64b48a4baae6f10f71b52
josue <josue.mlpro.2014@gmail.com>
2025-06-12 14:00:12 -0500

    ejercico_1-version_final-area del circulo

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -5,12 +5,16 @@ using namespace std;
 	
 int main(){
 	float r, pi, a;
-	cout<<ingrese el radio del circulo <<endl;
+	cout<<"ingrese el radio del circulo "<<endl;
 	cin>> r;
 	
 	pi= M_PI;
 	
-
+	a= pi* pow(r,2);
+	
+	cout<<"el area del circulo es: "<<endl;
+	cout<< a;
+	
 	
 	return 0;
 }
