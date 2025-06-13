63939ec61143808ef427edfbfc71469b86ab33b2
josue <josue.mlpro.2014@gmail.com>
2025-06-12 20:53:49 -0500

    ejercicio_4-version_final-raices de la ecuacion cuadratica

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -4,15 +4,37 @@ using namespace std;
 
 int main(){
 	
-	int a, b, c;
+	float a, b, c, d, x1, x2;
 	
-	cout<<ingrese el coeficiente de la variable de grado 2 <<endl;
+	cout<<"ingrese el coeficiente de la variable de grado 2 "<<endl;
 	cin>> a;
 	
-	cout<<ingrese el coeficiente de la variable de grado 1 <<endl;
+	cout<<"ingrese el coeficiente de la variable de grado 1 "<<endl;
 	cin>> b;
-	cout<<ingrese el termino independiente <<endl;
+	cout<<"ingrese el termino independiente "<<endl;
 	cin>> c;
 	
 	
+	// d= discriminante
+	d= pow(b, 2) - (4*a*c) ;
+	
+	if(a != 0){
+		
+		if(d>=0){
+			
+			x1= (-(b)+ (sqrt(d))) /(2*a);
+			
+			x2= (-(b) - (sqrt(d))) / (2*a);
+			
+			cout<<"las raices son: "<<endl;
+			cout<< x1<<endl;
+			cout<< x2;
+		}else{
+			cout<<"las raices son numeros imaginarios" ;
+		}
+	}else{
+		
+		cout<<"vuelve a intentarlo ";
+	}
+	return 0;
 }
