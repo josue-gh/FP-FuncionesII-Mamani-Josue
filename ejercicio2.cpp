8758673cae3b3d8dc95a99960771636c2c085697
josue <josue.mlpro.2014@gmail.com>
2025-06-12 20:18:44 -0500

    ejercicio_2-version_final-numero_aleatorio

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -4,14 +4,22 @@ using namespace std;
 
 int main(){
 	
-	int cn, mv;
-	cout<<ingrese la cantidad de numeros aleatorios que desea generar <<endl;
+	int cn, mv, na;
+	cout<<"ingrese la cantidad de numeros aleatorios que desea generar "<<endl;
 	cin>> cn;
 	
-	cout<<ingrese el maximo valor que puede tener el numero <<endl;
+	cout<<"ingrese el maximo valor que puede tener el numero "<<endl;
 	cin>> mv;
 	
+	cout<<"los numeros aleatorios son: "<<endl;
 	
+	for(int i=1; i<=cn; i++){
+		
+	na= rand() % mv ;
+	
+	cout<< na<<endl;
+	
+	}
 	
 	return 0;
 }
