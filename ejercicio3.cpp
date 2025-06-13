80a173a376b18d0a285dca96d5bf1b415550050c
josue <josue.mlpro.2014@gmail.com>
2025-06-12 20:30:20 -0500

    ejercicio_3-version_final-hallando la hipotenusa de un triangulo

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -3,15 +3,19 @@
 using namespace std;
 
 int main(){
-	int c1, c2, sc;
+	float c1, c2, sc, h;
 	
-	cout<<ingrese el primer cateto  <<endl;
+	cout<<"ingrese el primer cateto "<<endl;
 	cin>> c1;
 	
-	cout<<ingrese el segundo cateto <<endl;
+	cout<<"ingrese el segundo cateto "<<endl;
 	cin>> c2;
 	
 	sc= pow(c1, 2) + pow(c2, 2) ;
 	
+	h= sqrt(sc) ;
+	
+	cout<<"la hipotenusa es: "<<endl;
+	cout<< h;
 	return 0;
 }
