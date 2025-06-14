25f8054d9eb86207300749ac72c5b07e9c26831f
josue <josue.mlpro.2014@gmail.com>
2025-06-14 11:35:21 -0500

    ejercicio_8-avance_2-realizando la funcion main y una funcion secundaria

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -3,3 +3,44 @@
 #include<cstdlib>
 
 using namespace std;
+
+int tragamonedas(int s ){
+	return s*10;
+}
+
+
+
+
+
+int main(){
+	
+	int a, b, c, mon, apos;
+	string r;
+	
+	cout<<"juego del tragamoneda "<<endl;
+	cout<<"inicias con 100 monedas "<<endl;
+	
+	mon=100;
+	
+	do{
+		
+	cout<<"cuanto deseas apostar en esta ronda (1-10) "<<endl;
+	cin>> apos;
+	cout<< endl;
+		
+	a= rand( ) % 5+1;
+	b= rand( ) % 5+1;
+	c= rand( ) % 5+1;
+	
+	cout<<a<<"-"<<b<<"-"<<c<<endl;
+	
+	if(a==b and b==c and a==c){
+	
+	cout<<"has ganado: "<<tragamonedas(apos)<<endl;	
+	}
+		
+	cout<<"deseas seguir jugando? (si o no) "<<endl;
+	cin>>r;	
+	}while(r=="si" or mon>0);
+	
+}
